8d3048895aece8fc6211c7894482d1683bb0e615
laurencosgrove <laurenacosgrove@gmail.com>
2024-03-25 18:44:18 +0000

    about/profile started

---
+++ front-end/app/profile/page.tsx
@@ -0,0 +1,11 @@
+import Profile from "../components/Profile"
+
+export default function Home() {
+    return (
+      <>
+        <div className="m-10 ">
+            <Profile />
+        </div>
+      </>
+    )
+  }
