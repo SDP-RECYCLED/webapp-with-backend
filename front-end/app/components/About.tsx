fc8cf571258a416ebb82cf4af1be0f1b54537229
siddharthsakriya <76904146+siddharthsakriya@users.noreply.github.com>
2024-03-25 16:46:17 +0000

    git pull some bitches

--- front-end/app/components/About.tsx
+++ front-end/app/components/About.tsx
@@ -29,7 +29,7 @@ const About = () => {
                     <span className="font-bold">Julie Hong</span>
                     </h3>
                     <p className="text-gray-700">
-                    As part of the software team, Julie plays a crucial role in designing using Figma and developing the website's frontend using Next.js. She also contributes to market research efforts.</p>
+                    As part of the software team, Julie plays a crucial role in designing using Figma and developing the website&apos;s frontend using Next.js. She also contributes to market research efforts.</p>
                 </div>
                 <div>
                     <h3 className="text-lg font-semibold mb-2">
@@ -64,7 +64,7 @@ const About = () => {
                     <span className="font-bold">Siddharth Sakriya</span>
                     </h3>
                     <p className="text-gray-700">
-                    Siddharth is a versatile member, contributing to both hardware and software development. He led the website's backend development using Flask and ensured seamless integration with the frontend.</p>
+                    Siddharth is a versatile member, contributing to both hardware and software development. He led the website&apos;s backend development using Flask and ensured seamless integration with the frontend.</p>
                 </div>
                 <div>
                     <h3 className="text-lg font-semibold mb-2">
