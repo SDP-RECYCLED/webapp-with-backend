83384202fb2fa2ca068f8b2eb9ebc72f3978d129
siddharthsakriya <76904146+siddharthsakriya@users.noreply.github.com>
2024-03-26 17:26:18 +0000

    cors changeds

--- flask-backend/main.py
+++ flask-backend/main.py
@@ -2,6 +2,6 @@ from website import create_app
 from flask_cors import CORS
 
 app = create_app()
-CORS(app, supports_credentials=True, origins='http://client:3000')
+CORS(app, supports_credentials=True)
 if __name__ == '__main__':
     app.run(debug=False, host='0.0.0.0', port=8080)
